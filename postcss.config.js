c9ddf020639357552dbd9d91d480ab37c8320554
therealFIGBERT <figbert@figbert.com>
2020-05-24 15:06:40 -0700

    :wrench: Change PurgeCSS to include post and project styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const tailwindcss = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
+  content: ["./src/**/*.svelte", "./src/**/*.html", "./src/routes/posts/_posts.js", "./src/routes/projects/_projects.js"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 module.exports = {
