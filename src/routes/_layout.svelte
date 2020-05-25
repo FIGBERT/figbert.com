a4573796585fca2dd73c7cc632e7df033d350a32
therealFIGBERT <figbert@figbert.com>
2020-05-24 19:10:51 -0700

    :lipstick: Add margin to the bottom of all pages via _layout.svelte

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,7 +2,7 @@
 	import Nav from "../components/nav.svelte";
 </script>
 
-<main class="w-full sm:w-5/6 px-2 sm:px-0 mx-auto mt-12 flex space-x-12">
+<main class="w-full sm:w-5/6 px-2 sm:px-0 mx-auto mt-12 flex space-x-12 mb-12">
 	<Nav/>
 	<slot></slot>
 </main>
