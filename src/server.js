4c945487f395bb211d283bce987bce594164f7d6
therealFIGBERT <figbert@figbert.com>
2020-05-28 18:01:46 -0700

    :clown_face: Revert my shitty header commits
    This reverts commits:
    	3ee0ae2e3ca96ac07ca66447b0ab32c8099ef98e
    	46bc58b7de8e4214da4b4647c8218f873e80036e
    
    I'm going to eventually implement these headers, but I need to do some research in order to actually make it work with Netlify (or perhaps change hosts)

--- src/server.js
+++ src/server.js
@@ -1,50 +1,17 @@
 import sirv from 'sirv';
-import express from "express";
+import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-import { v4 as uuidv4 } from 'uuid';
-import helmet from "helmet";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = express();
-app.use(
-	compression({ threshold: 0 }),
-	sirv('static', { dev })
-);
-app.use((req, res, next) => {
-	res.locals.nonce = uuidv4();
-	next();
-});
-app.use(
-	helmet({
-		hsts: {
-			maxAge: 5184000,
-			includeSubDomains: true,
-			preload: true
-		},
-		contentSecurityPolicy: {
-			directives: {
-				scriptSrc: [
-					"'self'",
-					(req, res) => `'nonce-${res.locals.nonce}'`,
-					"https://gc.zgo.at"
-				],
-				imgSrc: [
-					"https://txtodo.goatcounter.com/count"
-				]
-			}
-		},
-		referrerPolicy: {
-			policy: "no-referrer"
-		},
-		frameguard: {
-			action: "deny"
-		}
-	})
-);
-app.use(sapper.middleware());
-app.listen(PORT, err => {
-	if (err) console.log("error", err);
-});
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
